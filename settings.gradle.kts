659b640bf07b3bcb2e2496340da71c0faa30d07a
Capslock800000 <mmzyd@outlook.com>
2026-08-08 10:17:58 +0800

    fix7

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -8,6 +8,7 @@ pluginManagement {
         id("com.android.application") version "9.2.1"
         id("com.android.library") version "9.2.1"
         id("org.jetbrains.kotlin.plugin.compose") version "2.1.0"
+        id("org.jetbrains.kotlin.plugin.parcelize") version "2.1.0"
     }
 }
 dependencyResolutionManagement {
