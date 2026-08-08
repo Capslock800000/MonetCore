474eda53485bfa80363fcf56225ff8856b35c042
Capslock800000 <mmzyd@outlook.com>
2026-08-08 13:23:28 +0800

    fix12

--- monet-service/build.gradle.kts
+++ monet-service/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.monettheme.service"
-    compileSdk = 36
+    compileSdk = 37
 
     defaultConfig {
         applicationId = "com.monettheme.service"
         minSdk = 28
-        targetSdk = 36
+        targetSdk = 37
         versionCode = 1
         versionName = "1.0.0"
     }
